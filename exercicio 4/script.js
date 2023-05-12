62567cf6c9cb86ac479ddf614266822aa3b3ad9e
JulioCarraro1 <130612671+JulioCarraro1@users.noreply.github.com>
2023-05-11 22:06:28 -0300

    Add files via upload

---
+++ exercicio 4/script.js
@@ -0,0 +1,9 @@
+let valorpago  = document.querySelector("valorpago");
+
+let valorreceber= document.querySelector("#valorreceber");
+
+let botão = document.querySelector("!#botão");
+
+let troco = document.querySelector("#troco");
+
+
