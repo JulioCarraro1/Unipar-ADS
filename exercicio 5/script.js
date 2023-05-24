cc8d6c69f0b0abd0f08ae49d01f7222b516d76ec
JulioCarraro1 <130612671+JulioCarraro1@users.noreply.github.com>
2023-05-24 20:32:11 -0300

    Add files via upload

---
+++ exercicio 5/script.js
@@ -0,0 +1,12 @@
+let Inputvlr01 = document.querySelector("#Inputvlr01");
+let inputvlr02 = document.querySelector("#inputvlr02");
+let btverificar = document.querySelector("#btverificar");
+let resultado = document.querySelector("#resultado");
+
+function verificarmaior(){
+    let vlr1 = Number(inputvlr01.value);
+    let vlr2 = Number(inputvlr01.value);
+    if (vlr1 > vlr2){
+        
+    }
+}
